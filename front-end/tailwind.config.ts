902b33cd06811424a4fc5287641a82b086b93cdd
Anand <anand@Anands-MacBook-Pro.local>
2024-09-27 16:03:09 +0800

    develop init

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -11,16 +11,17 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        main: "#00cc00",
       },
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: "1rem",
-        sm: "1rem",
-        lg: "1rem",
-        xl: "1rem",
-        "2xl": "1rem",
+        DEFAULT: "0.1rem",
+        sm: "0.1rem",
+        lg: "0.1rem",
+        xl: "0.1rem",
+        "2xl": "0.1rem",
       },
     },
   },
