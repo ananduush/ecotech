d909320c28da69e9aaf41bfd25ad3ecd36eb9d9c
Anand <bilguunanand04@gmail.com>
2024-10-02 04:31:54 +0800

    clean

--- front-end/src/i18n/routing.ts
+++ front-end/src/i18n/routing.ts
@@ -2,11 +2,10 @@ import { defineRouting } from "next-intl/routing";
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
 export const routing = defineRouting({
-	locales: ["mn", "en"],
+  locales: ["mn", "en"],
 
-	// Used when no locale matches
-	defaultLocale: "mn",
+  defaultLocale: "mn",
 });
 
 export const { Link, redirect, usePathname, useRouter } =
-	createSharedPathnamesNavigation(routing);
+  createSharedPathnamesNavigation(routing);
