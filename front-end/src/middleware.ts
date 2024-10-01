d7030110806497c2bda5c9bef7a6e8e98431888f
Tumenchudur <tumenchodor@gmail.com>
2024-10-02 04:26:50 +0800

    locale detection false

--- front-end/src/middleware.ts
+++ front-end/src/middleware.ts
@@ -1,9 +1,10 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export default createMiddleware(routing, {
+	localeDetection: false
+});
 
 export const config = {
-	// Match only internationalized pathnames
 	matcher: ["/", "/(en|mn)/:path*"],
 };
